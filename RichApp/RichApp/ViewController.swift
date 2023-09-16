5876ed4e872034ba7e620b2b7921a32b23d57294
Joy Hsieh <sy25789@gmail.com>
2023-09-16 16:18:00 +0800

    Initial Commit

---
+++ RichApp/RichApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichApp
+//
+//  Created by joy hsieh on 2023/9/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
